20cef19fc2058026c8b3304ff1ddd9363e38bd19
Devin Kapla <devinkapla@gmail.com>
2019-04-01 14:08:47 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { rootReducer } from './reducers'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root'));
+
+serviceWorker.unregister();
