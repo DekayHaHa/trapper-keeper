1ba4d320ea3ba83a550118427060d9a3be6900ed
Elizabeth Asbell <e.asbell8@gmail.com>
2019-04-02 20:43:26 -0600

    Add fetchItems action and reducer.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
-export const addTitle = (title) => ({
-  type: 'ADD_TITLE',
-  title
-}) 
+// export const addTitle = (title) => ({
+//   type: 'ADD_TITLE',
+//   title
+// }) 
 
-export const addItem = (item) => ({
-  type: 'ADD_ITEM',
-  item
+// export const addItem = (item) => ({
+//   type: 'ADD_ITEM',
+//   item
+// })
+
+export const fetchItems = (items) => ({
+  type: 'FETCH_ITEMS',
+  items
 })
--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,10 +1,8 @@
 export const items = (state = [], action) => {
   switch(action.type) {
-    case 'ADD_IDEA':
-      return [...state, {title: state.title, items: [...state.items, action.item]}]
-    case 'ADD_TITLE':
-      return [...state, {title: action.title}]
+    case 'FETCH_ITEMS':
+      return action.items;
     default: 
-      return state
+      return state;
   }
 }
