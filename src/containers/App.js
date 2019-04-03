fcccbe0d88851c52e71bcc55c1248b5d6ad52517
Isaac Sunoo <isaacsunoo@gmail.com>
2019-04-03 14:28:25 -0600

    added header and material ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Form from './Form';
 import { connect } from 'react-redux';
+import { Header } from '../components/Header';
 import { getNotes } from '../thunks/getNotes';
+import Form from './Form';
 
 class App extends Component {
   componentDidMount() {
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Keeper</h1>
+        <Header />
         <Form />
       </div>
     );
