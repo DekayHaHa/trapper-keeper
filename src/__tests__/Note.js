d86cc73a592d4e100b8dedf8a082565422e70474
Elizabeth Asbell <e.asbell8@gmail.com>
2019-04-04 14:31:22 -0600

    Add Note tests.

--- src/__tests__/Note.js
+++ src/__tests__/Note.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import { Note } from '../components/Note';
+import { Note, mapDispatchToProps } from '../components/Note';
 import { shallow } from 'enzyme';
+import { deleteNote } from '../thunks/deleteNote';
+
+jest.mock('../thunks/deleteNote');
 
 describe('Note', () => {
+  let mockItemsList;
+  let wrapper;
+  let mockFn;
+
+  beforeEach(() => {
+    mockFn = jest.fn();
+    mockItemsList = [{text: "item", isComplete: false, id: 1554329952339}]
+    wrapper = shallow(<Note title={'title'} itemsList={mockItemsList} deleteNote={mockFn}/>);
+  });
+
   it('should match snapshot with all data passed in', () => {
-    const mockItemsList = [{text: "item", isComplete: false, id: 1554329952339}]
-    let wrapper = shallow(<Note title={'title'} itemsList={mockItemsList} />);
     expect(wrapper).toMatchSnapshot();
   });
-})
+
+  it('should invoke deleteNote on removeNote', () => {
+    wrapper.instance().removeNote();
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with a deleteNote action', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = deleteNote(2);
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.deleteNote(2);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  });
+});
